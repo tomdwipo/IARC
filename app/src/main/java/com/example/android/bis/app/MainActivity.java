a93e1fdd2a156e6d0327d0da51b4061288d8ca99
tommyputranto <tommyputranto1@gmail.com>
2015-04-14 11:39:25 +0700

    vb

--- app/src/main/java/com/example/android/bis/app/MainActivity.java
+++ app/src/main/java/com/example/android/bis/app/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
           ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-        // Assign adapter to ListView
+        /// Assign adapter to ListView
         listView.setAdapter(adapter);
 
 
